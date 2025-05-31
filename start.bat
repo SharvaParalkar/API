097a52c79ed8a12429995cf8fcb0d7573adb773c
sharva paralkar <sharva.paralkar@gmail.com>
2025-05-31 13:03:41 -0500

    add start.bat

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+
+start wt ^
+    new-tab -d "C:\Users\Admin\Downloads\API\dashboard" cmd /k "node dashboard.js" ^
+    ; new-tab -d "C:\Users\Admin\Downloads\API\DB" cmd /k "node server.js" ^
+    ; new-tab -d "C:\Users\Admin\Downloads\API\Order-Form" cmd /k "node server.js" ^
+    ; new-tab -d "C:\Users\Admin\Downloads\API\STL-UPLOADER" cmd /k "node server.js" ^
+    ; new-tab -d "C:\Users\Admin" cmd /k "cloudflared tunnel run stl-api"
