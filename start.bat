b71e654cb84d4fccf1caafd6732954f36075a47b
sharva paralkar <sharva.paralkar@gmail.com>
2025-06-16 13:31:00 -0500

    dashboard set up for filament

--- start.bat
+++ start.bat
@@ -5,4 +5,5 @@ start wt ^
     ; new-tab -d "C:\Users\Admin\Downloads\API\DB" cmd /k "node server.js" ^
     ; new-tab -d "C:\Users\Admin\Downloads\API\Order-Form" cmd /k "node server.js" ^
     ; new-tab -d "C:\Users\Admin\Downloads\API\STL-UPLOADER" cmd /k "node server.js" ^
+    ; new-tab -d "C:\Users\Admin\Downloads\API\Filament" cmd /k "node server.js" ^
     ; new-tab -d "C:\Users\Admin" cmd /k "cloudflared tunnel run stl-api"
